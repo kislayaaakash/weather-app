6490c44cc037f9377b18d9551f4e36d2d4cf4785
kislayaaakash <60338788+kislayaaakash@users.noreply.github.com>
2025-01-20 05:12:46 +0530

    Update AppConfig.java

--- backend/src/main/java/com/ps/weatherapp/configurations/AppConfig.java
+++ backend/src/main/java/com/ps/weatherapp/configurations/AppConfig.java
@@ -22,8 +22,15 @@ public class AppConfig {
     }
 
     @Bean
-    public Map<String, CityWeatherDetails> weatherBackUpData(FileManager<CityWeatherDetails> fileManager, @Value("${service.cacheFileName}") String fileName) {
-        Map<String, CityWeatherDetails> dataMap = fileManager.loadDataFromFile(fileName, new TypeReference<>() {});
-        return Collections.synchronizedMap(dataMap);  // Wrap the map to make it thread-safe
+    public Map<String, CityWeatherDetails> weatherBackUpData(FileManager<CityWeatherDetails> fileManager) {
+        try {
+            Map<String, CityWeatherDetails> dataMap = fileManager.loadDataFromFile("weatherCache.json", new TypeReference<>() {
+            });
+            return Collections.synchronizedMap(dataMap);  // Wrap the map to make it thread-safe
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            logger.error(e.getMessage());
+            return Collections.emptyMap();
+        }
     }
 }
