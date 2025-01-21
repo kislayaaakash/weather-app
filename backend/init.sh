972f5d441db5287c91a60555cefd45ca738e715a
KISLAYA AAKASH <kislayaaakash@gmail.com>
2025-01-22 01:02:24 +0530

    init.sh line break remove

--- backend/init.sh
+++ backend/init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 # Define the path to the file
 CACHE_FILE=/app/weatherCache.json
-
 # Check if the file exists; create it if not
 if [ ! -f "$CACHE_FILE" ]; then
   echo "{}" > "$CACHE_FILE" # Initialize with an empty JSON object
@@ -10,6 +8,5 @@ if [ ! -f "$CACHE_FILE" ]; then
 else
   echo "weatherCache.json file already exists."
 fi
-
 # Start the Spring Boot application
 java -jar /app/weatherapp.jar
