997c89629998a49224feab66f5a063446c77ae98
KISLAYA AAKASH <kislayaaakash@gmail.com>
2025-01-22 02:15:47 +0530

    inti.sh restore

--- backend/intial.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CACHE_FILE=/app/weatherCache.json
-if [ ! -f "$CACHE_FILE" ]; then
-  echo "{}" > "$CACHE_FILE" # Initialize with an empty JSON object
-  echo "Created weatherCache.json file."
-else
-  echo "weatherCache.json file already exists."
-fi
-java -jar /app/weatherapp.jar # Run jar
