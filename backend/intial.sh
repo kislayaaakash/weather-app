f7ac75a0860dd12c13ff4e22dbd847bfa474d8a2
KISLAYA AAKASH <kislayaaakash@gmail.com>
2025-01-22 02:04:33 +0530

    intial.sh removed comments

--- backend/intial.sh
+++ backend/intial.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-# Define the path to the file
 CACHE_FILE=/app/weatherCache.json
 
-# Check if the file exists; create it if not
 if [ ! -f "$CACHE_FILE" ]; then
   echo "{}" > "$CACHE_FILE" # Initialize with an empty JSON object
   echo "Created weatherCache.json file."
@@ -11,5 +8,4 @@ else
   echo "weatherCache.json file already exists."
 fi
 
-# Start the Spring Boot application
 java -jar /app/weatherapp.jar
