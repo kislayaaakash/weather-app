f637ff8b4a79e242713ac961fffc677d853da14c
KISLAYA AAKASH <kislayaaakash@gmail.com>
2025-01-22 02:10:36 +0530

    intial.sh 1

--- backend/intial.sh
+++ backend/intial.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 CACHE_FILE=/app/weatherCache.json
-
 if [ ! -f "$CACHE_FILE" ]; then
   echo "{}" > "$CACHE_FILE" # Initialize with an empty JSON object
   echo "Created weatherCache.json file."
 else
   echo "weatherCache.json file already exists."
 fi
-
-java -jar /app/weatherapp.jar
+java -jar /app/weatherapp.jar # Run jar
