147bacabbb61081bc2c5ec6c628ea2053f2e7cee
KISLAYA AAKASH <kislayaaakash@gmail.com>
2025-01-22 13:08:28 +0530

    remove init.sh

--- backend/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CACHE_FILE=/app/weatherCache.json
-if [ ! -f "$CACHE_FILE" ]; then
-  echo "{}" > "$CACHE_FILE"
-  echo "Created weatherCache.json file."
-else
-  echo "weatherCache.json file already exists."
-fi
-java -jar /app/weatherapp.jar
